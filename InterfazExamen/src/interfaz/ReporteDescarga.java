c161807e66a6ffe1c1e397a3f439afe3518ffa84
Alberto Sánchez <albertoj1606@gmail.com>
2019-05-20 18:57:45 -0500

    Se añadió clase de reporte

---
+++ InterfazExamen/src/interfaz/ReporteDescarga.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz;
+
+import java.util.*;
+
+/**
+ *
+ * @author Alberto Sánchez
+ */
+public class ReporteDescarga {
+ 
+  private int id;
+  private String nombreHost;
+  private Date fechaDescargas;
+  private String URL;
+
+  public ReporteDescarga(int id, String nombreHost, Date fechaDescargas, String URL) {
+    this.id = id;
+    this.nombreHost = nombreHost;
+    this.fechaDescargas = fechaDescargas;
+    this.URL = URL;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getNombreHost() {
+    return nombreHost;
+  }
+
+  public Date getFechaDescargas() {
+    return fechaDescargas;
+  }
+
+  public String getURL() {
+    return URL;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setNombreHost(String nombreHost) {
+    this.nombreHost = nombreHost;
+  }
+
+  public void setFechaDescargas(Date fechaDescargas) {
+    this.fechaDescargas = fechaDescargas;
+  }
+
+  public void setURL(String URL) {
+    this.URL = URL;
+  }
+
+}
