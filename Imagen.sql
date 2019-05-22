e202f461867239e9dd2626d19ff496a770db3b87
Fer cabrera <soy-yonotu@hotmail.com>
2019-05-21 21:56:05 -0500

    DB

---
+++ Imagen.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IMAGEN (IDENTIFICADOR INTEGER DEFAULT AUTOINCREMENT: start 1 increment 1  NOT NULL GENERATED ALWAYS AS IDENTITY, NOMBRE VARCHAR(255), URL VARCHAR(300), PRIMARY KEY (IDENTIFICADOR));
+
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRivmLWgLM0PEyuvm-V1MZz0NFdx5L_FVA3zTetcbUx4NKGabdh');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 2', 'https://mondrian.mashable.com/uploads%252Fcard%252Fimage%252F640145%252F4f6bf4e0-8921-4169-87ca-2ae56c9cf5be.jpg%252F950x534__filters%253Aquality%252890%2529.jpg?signature=vN7lrDvBVyxOWhIBIVFOwJhAaTo=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 3', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUQZDNKc-zKvFg7JJRVVUdmW7Qb-Aadm1TnEEzHVWp6EANPf_nbw');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 4', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm2f9w5fRz1ZeI9uctxSFbbmRC2BCrVk10T2XSxdS5Aw7I7tM0');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 5', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgRFCk5-Aaqk880qlGroO3AZdzy0knslvSKwJPMx8VApqtNxit4g');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 6', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYphEqgesFOpDkrCkpWgnL3G-nN5QIVCMpl-3Rg4ZPdOVwxg9B');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 7', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq0onbn-UVguMQM1Bzr28Xqo4nMyVArFimfS3SMb0D_joXcjQ6');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 8', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcGiUiMV6vvTOmTi7Vjk9uEd9lnVj4C2Ioa2UvfTRga_PdVxHy');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 10', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRivmLWgLM0PEyuvm-V1MZz0NFdx5L_FVA3zTetcbUx4NKGabdh');
+INSERT INTO APP.IMAGEN (NOMBRE, URL) 
+	VALUES ('imagen 9', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMJPVf9-KjFk05_Tnyuiy1Y3poNI0gRDTEinwDBrZHCArqDNH0');
